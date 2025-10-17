80cf2058c7ac390d3c9c6bdf6aac3d9f519dd28c
himel2535 <monwarhossanhimel@gmail.com>
2025-10-18 00:37:40 +0600

    Font & Date-FNS Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from "../assets/logo.png"
+import React from "react";
+import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
-    return (
-        <div className='flex flex-col items-center justify-center gap-3'>
-            <img src={logo} alt="" className='mt-10'/>
-            <p>Journalism Without Fear or Favour</p>
-            <button className='btn btn-primary'>click me</button>
-        </div>
-    );
+  const date = new Date();
+  const weekday = format(date, "EEEE");
+  const rest = format(date, "MMMM dd, yyyy");
+  return (
+    <div className="flex flex-col items-center justify-center gap-3">
+      <img src={logo} alt="" className="mt-10" />
+      <p className="text-accent-content">Journalism Without Fear or Favour</p>
+      <p className="text-semibold text-accent">
+        <span className="text-primary">{weekday }  </span>
+        <span> {rest}</span>
+      </p>
+    </div>
+  );
 };
 
 export default Header;
