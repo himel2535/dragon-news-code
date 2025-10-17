39a96d2ba9c4821d9f0c59e93ae4567b30315af8
himel2535 <monwarhossanhimel@gmail.com>
2025-10-17 23:12:56 +0600

    Route Set-up, theme set up & header Design

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Dragon News Code</div>,
-  },
-]);
+import router from "./Routes/Router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}>
-      <App />
     </RouterProvider>
   </StrictMode>
 );
